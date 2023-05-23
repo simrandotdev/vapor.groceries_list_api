e082d8004ddb4d846dbe030519925c70b83305c9
Simran Preet Singh <simran5590@gmail.com>
2023-05-22 17:11:51 -0700

    Added `CreateUserMigration`

---
+++ Sources/App/Migrations/CreateUserMigration.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Simran Preet Narang on 2023-05-22.
+//
+
+import Foundation
+import Fluent
+
+struct CreateUserMigration: AsyncMigration {
+    
+    func prepare(on database: FluentKit.Database) async throws {
+        
+        try await database
+            .schema("users")
+            .field("username", .string, .required).unique(on: "username")
+            .field("password", .string, .required)
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        
+        try await database
+            .schema("users")
+            .delete()
+    }
+    
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,18 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // Database setup
+    try app.databases.use(.postgres(url: "postgres://mvofhhql:AIs5MHoy8rQUmofq10IX0dDoJuJaw7pJ@mahmud.db.elephantsql.com/mvofhhql"), as: .psql)
+    
+    // Migrations
+    app.migrations.add(CreateUserMigration())
+    
     // register routes
     try routes(app)
 }
